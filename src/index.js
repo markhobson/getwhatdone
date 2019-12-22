17beb8a39bf6135c66c3403c50956105ced0b11a
Mark Hobson <markhobson@gmail.com>
2019-12-22 15:18:16 +0000

    Randomise noun

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 const path = require('path');
+const nouns = require('noun-json');
 const puppeteer = require('puppeteer');
 
 (async () => {
 	const browser = await puppeteer.launch();
-	const page = await browser.newPage();
 	
+	const page = await browser.newPage();
 	await page.setViewport({width: 1280, height: 720});
 	await page.goto(`file:${path.join(__dirname, 'index.html')}`, {waitUntil: 'networkidle0'});
+	
+	const noun = nouns[Math.floor(Math.random() * nouns.length)];
+	await page.$eval('#noun', (element, text) => element.innerHTML = text, noun);
+	
 	await page.screenshot({path: 'screenshot.png'});
 	
 	await browser.close();
