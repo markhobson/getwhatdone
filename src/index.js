0db7e6bda3d52463d2595d8abf3384fb9f3319d6
Mark Hobson <markhobson@gmail.com>
2019-12-26 17:29:57 +0000

    Tweet screenshot

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
+require('dotenv').config();
 const path = require('path');
+const fs = require('fs');
 const nouns = require('noun-json');
 const puppeteer = require('puppeteer');
+const Twitter = require('twitter');
+
+async function tweet(path) {
+	const client = new Twitter({
+		consumer_key: process.env.TWITTER_CONSUMER_KEY,
+		consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+		access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+		access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+	});
+	const data = fs.readFileSync(path);
+	const media = await client.post('media/upload', {media: data});
+	await client.post('statuses/update', {media_ids: media.media_id_string});
+}
 
 (async () => {
 	const browser = await puppeteer.launch();
@@ -15,4 +30,6 @@ const puppeteer = require('puppeteer');
 	await page.screenshot({path: 'screenshot.png'});
 	
 	await browser.close();
+	
+	await tweet('screenshot.png');
 })();
