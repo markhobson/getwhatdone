3d6d4da5d52fe715ac03f6b575a0170da861eb46
Mark Hobson <markhobson@gmail.com>
2019-12-26 17:42:18 +0000

    Extract create image function

--- src/index.js
+++ src/index.js
@@ -5,31 +5,35 @@ const nouns = require('noun-json');
 const puppeteer = require('puppeteer');
 const Twitter = require('twitter');
 
-async function tweet(path) {
+async function createImage(noun) {
+	const pagePath = path.join(__dirname, 'index.html');
+	const imagePath = 'screenshot.png';
+
+	const browser = await puppeteer.launch();
+	const page = await browser.newPage();
+	await page.setViewport({width: 1280, height: 720});
+	await page.goto(`file:${pagePath}`, {waitUntil: 'networkidle0'});
+	await page.$eval('#noun', (element, text) => element.innerHTML = text, noun);
+	await page.screenshot({path: imagePath});
+	await browser.close();
+	
+	return imagePath;
+}
+
+async function tweet(status, mediaPath) {
 	const client = new Twitter({
 		consumer_key: process.env.TWITTER_CONSUMER_KEY,
 		consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
 		access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
 		access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 	});
-	const data = fs.readFileSync(path);
-	const media = await client.post('media/upload', {media: data});
-	await client.post('statuses/update', {media_ids: media.media_id_string});
+	const mediaData = fs.readFileSync(mediaPath);
+	const media = await client.post('media/upload', {media: mediaData});
+	await client.post('statuses/update', {status: status, media_ids: media.media_id_string});
 }
 
 (async () => {
-	const browser = await puppeteer.launch();
-	
-	const page = await browser.newPage();
-	await page.setViewport({width: 1280, height: 720});
-	await page.goto(`file:${path.join(__dirname, 'index.html')}`, {waitUntil: 'networkidle0'});
-	
 	const noun = nouns[Math.floor(Math.random() * nouns.length)];
-	await page.$eval('#noun', (element, text) => element.innerHTML = text, noun);
-	
-	await page.screenshot({path: 'screenshot.png'});
-	
-	await browser.close();
-	
-	await tweet('screenshot.png');
+	const imagePath = await createImage(noun);
+	await tweet(`Get ${noun} done`, imagePath);
 })();
