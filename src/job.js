815cc61ed15cf02937971d83f565cafe0138ffe9
Mark Hobson <markhobson@gmail.com>
2020-01-02 13:31:28 +0000

    Extract page modifier

--- src/job.js
+++ src/job.js
@@ -5,16 +5,21 @@ const puppeteer = require('puppeteer');
 const nouns = require('noun-json');
 const Twitter = require('twitter');
 
-async function createImage(pagePath, noun, imagePath) {
+async function createImage(pagePath, pageModifier, imagePath) {
 	const browser = await puppeteer.launch({args: ['--no-sandbox']});
 	const page = await browser.newPage();
 	page.on('console', msg => console.log(`[pptr] ${msg.text()}`));
 	await page.setViewport({width: 1280, height: 720});
 	await page.goto(`file:${pagePath}`, {waitUntil: 'load'});
 	await page.waitForFunction('document.fonts.ready');
-	await page.$eval('#noun', (element, text) => element.innerHTML = text, noun);
+	await pageModifier(page);
 	await page.screenshot({path: imagePath});
 	await browser.close();
+}
+
+async function setNoun(noun) {
+	return async page =>
+		await page.$eval('#noun', (element, text) => element.innerHTML = text, noun);
 }
 
 async function tweet(status, mediaPath) {
@@ -35,7 +40,7 @@ async function tweet(status, mediaPath) {
 	const imagePath = 'screenshot.png';
 	
 	try {
-		await createImage(pagePath, noun, imagePath);
+		await createImage(pagePath, await setNoun(noun), imagePath);
 		await tweet(`Get ${noun} done`, imagePath);
 		console.log(`Get ${noun} done`);
 	}
